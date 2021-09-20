f4c53719694ba2f9edc6eff4531ebd0d70747bb4
Samuel Alev <samuel.alev@protonmail.com>
2021-09-20 13:48:12 +0200

    feat: add prefer template rule

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -7,6 +7,8 @@ module.exports = {
   rules: {
     "linebreak-style": ["error", "unix"],
     "prettier/prettier": "error",
+    "prefer-template": "error",
+    "no-useless-concat": "error",
   },
   plugins: ["html", "prettier"],
   extends: ["plugin:prettier/recommended"],
